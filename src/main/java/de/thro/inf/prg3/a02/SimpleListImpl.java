8243699cf9121d381f53009694c9b169ef7d24ca
Carolime <ariesu@outlook.de>
2018-10-11 15:13:30 +0200

    Init of prog3 exc_02

--- src/main/java/de/thro/inf/prg3/a02/SimpleListImpl.java
+++ src/main/java/de/thro/inf/prg3/a02/SimpleListImpl.java
@@ -1,11 +1,77 @@
 package de.thro.inf.prg3.a02;
 
+import java.util.Iterator;
+
 /**
  * @author Peter Kurfer
  * Created on 10/6/17.
  */
-public class SimpleListImpl implements SimpleList {
+public class SimpleListImpl implements SimpleList
+{
+    /**
+     * Globale Variablen
+     */
+    static Element head = null;
+    static Element current = null;
 
-	// TODO: Implement the required methods.
+    /**
+     * Kettenglieder der Verketetenliste
+     */
+    private static class Element
+    {
+        Object item;
+        Element next;
+
+        public Element getNext() {
+            return next;
+        }
+
+        public void setNext(Element next) {
+            this.next = next;
+        }
+    }
+
+    /**
+     * Iterator Innereklasse
+     */
+    private static class SimpleIteratorImpl implements Iterator
+    {
+        @Override
+        public boolean hasNext() {
+
+            if(SimpleListImpl.current.getNext().equals(null))
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        @Override
+        public Object next() {
+            Object buffer = new Element();
+            buffer = SimpleListImpl.current;
+            current = current.getNext();
+
+            return buffer;
+        }
+    }
+
+    @Override
+    public void add(Object o) {
+
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public SimpleList filter(SimpleFilter filter) {
+        return null;
+    }
 
 }
